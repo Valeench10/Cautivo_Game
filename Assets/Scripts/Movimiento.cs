c37888354f0a8f8a7279115f3d67a007cf5e4d7d
Valeench10 <32395262+Valeench10@users.noreply.github.com>
2017-10-03 19:51:52 -0500

    V.1 Cautivo
    Movimiento del personaje.

---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public float movementSpeed = 10;
+    public float rotationSpeed = 60;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        //Movimiento
+        Vector3 movement = new Vector3(0, 0, vertical)* Time.deltaTime * movementSpeed;
+        transform.Translate(movement);
+
+        //Rotación
+        Vector3 rotation = new Vector3(0,horizontal, 0) * rotationSpeed * Time.deltaTime;
+        transform.Rotate(rotation);
+
+     
+    }
+}
