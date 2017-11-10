6b25caaf62d226be51079dce390ca4de85100fc7
Valeench10 <32395262+Valeench10@users.noreply.github.com>
2017-11-10 09:20:54 -0500

    Cautivo_V20
    Hola

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class Door : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject Key;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+    }
 }
